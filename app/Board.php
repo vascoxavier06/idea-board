8581fad36287ca5aa95f6a734a5201bf5aaebbdd
vasco xavier <vliscoxavier06@gmail.com>
2018-05-16 01:59:45 +0200

    creating relations between models

--- app/Board.php
+++ app/Board.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Board extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'user_id'
+    ];
+
+
+    public function user()
+    {
+        return $this->belongsTo('IdeaBoard\User');
+    }
+
+    public function ideas()
+    {
+        return $this->hasMany('IdeaBoard\Idea');
+    }
 }
--- app/Idea.php
+++ app/Idea.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Idea extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'description',
+        'board_id'
+    ];
+
+    public function board() {
+        return $this->belongsTo('IdeaBoard\Board');
+    }
 }
