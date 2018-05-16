5fde7be8e31e97a39e3ed8817b1742f12c01e62e
vasco xavier <vliscoxavier06@gmail.com>
2018-05-16 02:02:50 +0200

    redirecting user to dashboard when attempting to go to index

--- app/Http/Middleware/IsAuthenticated.php
+++ app/Http/Middleware/IsAuthenticated.php
@@ -3,24 +3,22 @@
 namespace IdeaBoard\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Facades\Auth;
+use Auth;
 
-class RedirectIfAuthenticated
+class IsAuthenticated
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
-     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next, $guard = null)
+    public function handle($request, Closure $next)
     {
-        if (Auth::guard($guard)->check()) {
+        if(Auth::check()) {
             return redirect('/dashboard');
         }
-
         return $next($request);
     }
 }
